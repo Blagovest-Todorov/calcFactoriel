64ebb33f51399c59e2a178d65260e7ed685e6a5f
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-13 13:09:11 +0200

    add a file CalcFactoriel

---
+++ CalcFactoriel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace Factoriel
+{
+    class CalcFactoriel
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());  // read number from console
+            BigInteger result = 1;  // lets result be of a type BigInteger to avoid  small type limits
+
+            for (int i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
