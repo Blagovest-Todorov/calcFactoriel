9304c7479eb6f29b45cbd69d05fc2b657ea480e1
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-13 13:17:47 +0200

    optimizing the algorythm in the forCycle

--- CalcFactoriel.cs
+++ CalcFactoriel.cs
@@ -9,9 +9,9 @@ namespace Factoriel
         {
             int number = int.Parse(Console.ReadLine());  // read number from console
             BigInteger result = 1;  // lets result be of a type BigInteger to avoid  small type limits
-
-            for (int i = 1; i <= number; i++)
-            {
+                         
+            for (int i = 2; i <= number; i++)    // We can start multiplying from 2 to save one unnecessay  
+            {                                    // cycle and multiplication of  1;
                 result = result * i;
             }
             Console.WriteLine(result);
